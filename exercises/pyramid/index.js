270ec3c165a7520805a635a97cd8b3ca299d12ca
shunsukw <ww.shunsuke@gmail.com>
2018-08-14 14:29:21 +0900

    in progress

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = "") {
+    if (n === row) {
+        return
+    }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level)
+        return pyramid(n, row + 1)
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2)
+
+    if (level.length >= midpoint - row && level.length <= midpoint + row) {
+        level += "#"
+    } else {
+        level += " "
+    }
+
+    pyramid(n, row, level)
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//     const midpoint = Math.floor((2 * n - 1) / 2)
+// 
+//     for (let row = 0; row < n; row++) {
+//         let level = ""
+// 
+//         for (let column = 0; column < 2 * n - 1; column++) {
+//             if (column >= midpoint - row && column <= midpoint + row) {
+//                 level += "#"
+//             } else {
+//                 level += " "
+//             }
+//         }
+//         console.log(level)
+//     }
+// }
